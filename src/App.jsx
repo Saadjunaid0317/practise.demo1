cf3505e00ef4251917f01b8d6ec937c27a236fc7
Saadjunaid0317 <msaadjunaid2000@gmail.com>
2025-01-25 21:00:02 +0500

    api4

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
 
-  const fetchApiData = async (name) => {
+  const fetchApiData = async () => {
     try {
-
-      const response = await axios.get(`https://api.genderize.io?name=${name}`);
+      const response = await axios.get(`https://api.ipify.org?format=json`);
+      console.log("response", response);
       setData(response.data);
     } catch (error) {
+      console.log("error", error.message);
       setError(error.message);
-    } finally {
-      setLoading(false); // Turn off loading state
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name.trim()) {
-      fetchApiData(name);
-    } else {
-      setError('Please enter a valid name');
-    }
-  };
+  useEffect(() => {
+    fetchApiData();
+  }, []);
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Enter a name to guess the gender"
-          aria-label="Enter a name"
-        />
-        <button type="submit" disabled={!name.trim()}>
-          Enter
-        </button>
-      </form>
-
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p style={{ color: 'red' }}>{error}</p>
+      {error ? (
+        <p>Oops! Something went wrong: {error}</p>
       ) : (
         data && (
-          <div>
-            <p>The gender of "{name}" is {data.gender}.</p>
-          </div>
+          <p>
+            {data.ip}
+          </p>
         )
       )}
     </>
